a7c68c52c2bc56d7c743e60ccba7b43163b1c0d4
Joao Souza <joaoosouza07@gmail.com>
2022-02-01 22:40:19 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,31 @@
+import { FiLink } from 'react-icons/fi';
+import logo from '../../assets/logo.png';
+
+import './home.css';
+
 export default function Home() {
     return(
-      <div>
-        <h1>HOME</h1>
+      <div className="container-home">
+        
+        <div className="logo">
+          <img src={logo} alt="logo" />
+
+          <h2>MyLink</h2>
+          <span>Cole seu link para encurtar 👇</span>
+        </div>
+
+        <div className="area-input">
+          <div>
+            <FiLink size={24} color='#FFF' />
+            <input 
+              type='text'
+              placeholder='Cole seu link aqui...'
+            />
+          </div>
+
+          <button>Gerar Link</button>
+        </div>
+
       </div>
     );
 }
