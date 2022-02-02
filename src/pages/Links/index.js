5b7d7ab43c31d43b68a8a7a506ca1a5fba91e107
Joao Souza <joaoosouza07@gmail.com>
2022-02-01 23:07:06 -0300

    Page Links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,7 +1,40 @@
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 export default function Links() {
     return(
-      <div>
-        <h1>Página de Links</h1>
+      <div className="links-container">
+        
+        <div className="links-header">
+          <Link to="/">
+            <FiArrowLeft size={30} color="#FFF" />
+          </Link>
+          <h2>Meus Links</h2>
+        </div>
+
+        <div className="links-item">
+          <button className="link">
+            <FiLink size={18} color="#FFF" />
+            https://www.google.com/
+          </button>
+
+          <button className="link-delete">
+            <FiTrash size={18} color="#dc3545" />
+          </button>
+        </div>
+
+        <div className="links-item">
+          <button className="link">
+            <FiLink size={18} color="#FFF" />
+            https://www.google.com/
+          </button>
+
+          <button className="link-delete">
+            <FiTrash size={18} color="#dc3545" />
+          </button>
+        </div>
+
       </div>
     );
 }
